2ad5d22dc4e008952411c87b4d21003ba4d65a66
Zhiqian-Zhang <zhiqianz29@gmail.com>
2022-06-30 23:12:10 -0700

    Fix firebase.utils.js

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -37,9 +37,14 @@ export const signInWithGooglePopup = () => {
 
 export const db = getFirestore(); 
 
+
 export const createUserDocumentFromAuth = async (userAuth) => {
     const userDocRef = doc(db, 'users', userAuth.uid);
     const userSnapshot = await getDoc(userDocRef);
+
+    if (!userSnapshot.exists()) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
 
         try {
             await setDoc(userDocRef, {
@@ -51,6 +56,8 @@ export const createUserDocumentFromAuth = async (userAuth) => {
             console.log('error creating the user', error.message);
         }
     }
-
     return userDocRef;
-}
+};
+
+    
+
